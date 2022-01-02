e8e29080b4f6f009b26343cb3990703cdda4dbaf
Anton Antonov <91306218+MagicFirework@users.noreply.github.com>
2022-01-02 12:29:07 +0600

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss          = require('gulp-sass')(require('sass'));
+const concat        = require('gulp-concat');
+const autoprefixer  = require('gulp-autoprefixer');
+const uglify        = require('gulp-uglify');
+const imagemin      = require('gulp-imagemin');
+const del           = require('del');
+const browserSync   = require('browser-sync').create();
+
+function browsersync() {
+  browserSync.init({
+    server: {
+        baseDir: "app/"
+    },
+    notify: false
+  })
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+  .pipe(scss({outputStyle: 'compressed'}))
+  .pipe(concat('style.min.css'))
+  .pipe(autoprefixer({
+    overrideBrowserslist: ['last 10 versions']
+  }))
+  .pipe(dest('app/css'))
+  .pipe(browserSync.stream())
+}
+
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+    'app/js/main.js'
+  ])
+  .pipe(uglify())
+  .pipe(concat('main.min.js'))
+  .pipe(dest('app/js'))
+  .pipe(browserSync.stream())
+}
+
+function images(){
+  return src('app/images/**/*.*')
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.mozjpeg({quality: 75, progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+      plugins: [
+        {removeViewBox: true},
+        {cleanupIDs: false}
+      ]
+    })
+  ]))
+  .pipe(dest('dist/images'))
+}
+
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/fonts/*.*',
+    'app/js/main.min.js'
+  ], {base: 'app'})
+  .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return del('dist')
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change',browserSync.reload)
+}
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
+
+exports.default = parallel(styles, scripts, browsersync, watching);
