6f26c5786d111932a6e4cb2d429fa41718587329
Anton Antonov <91306218+MagicFirework@users.noreply.github.com>
2022-01-15 20:26:37 +0600

    added 20

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: 'fade',
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
 });
