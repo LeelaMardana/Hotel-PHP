305e324eae05aa441590794e745b72cc364697d8
Anton Antonov <91306218+MagicFirework@users.noreply.github.com>
2022-01-27 13:16:45 +0600

    added send.php

---
+++ send.php
@@ -0,0 +1,62 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+  $mail->isSMTP();
+  $mail->CharSet = "UTF-8";
+  $mail->SMTPAuth   = true;
+  //$mail->SMTPDebug = 2;
+  $mail->Debugoutput = function ($str, $level) {
+    $GLOBALS['status'][] = $str;
+  };
+
+  // Настройки вашей почты
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'dinkurumi@gmail.com'; // Логин отправителя
+  $mail->Password   = 'Wow6464d99www709'; // Пароль отправителя
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+  $mail->setFrom('dinkurumi@gmail.com', 'Anton Antonov'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('deankurumi@gmail.com');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {
+    $result = "success";
+  } else {
+    $result = "error";
+  }
+} catch (Exception $e) {
+  $result = "error";
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
